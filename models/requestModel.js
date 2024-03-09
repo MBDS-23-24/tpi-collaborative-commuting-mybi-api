80111da2b935ba09c91d6046dc950b92c27637e6
Badis Boucheffa <21914662@etu.unicaen.fr>
2024-03-09 11:50:09 +0100

    Add request model

---
+++ models/requestModel.js
@@ -0,0 +1,46 @@
+// Define the Request model structure
+const Request = {
+    userId: {
+        type: 'INTEGER',
+        primaryKey: true,
+        allowNull: false,
+        field: 'UserID'
+    },
+    originLat: {
+        type: 'DOUBLE',
+        allowNull: false,
+        field: 'OriginLat'
+    },
+    originLong: {
+        type: 'DOUBLE',
+        allowNull: false,
+        field: 'OriginLong'
+    },
+    destinationLat: {
+        type: 'DOUBLE',
+        allowNull: false,
+        field: 'DestinationLat'
+    },
+    destinationLong: {
+        type: 'DOUBLE',
+        allowNull: false,
+        field: 'DestinationLong'
+    },
+    time: {
+        type: 'DATE',
+        defaultValue: Date.now(),
+        field: 'Time'
+    },
+    status: {
+        type: 'STRING', // Adjust the data type based on your requirements
+        allowNull: false,
+        field: 'Status'
+    },
+    type: {
+        type: 'STRING',
+        allowNull: false,
+        field: 'Type' // Add the Type field
+    }
+};
+
+module.exports = Request;
